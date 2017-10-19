84be7e65371a89e8a08b2c61d5f4f470ad54c772
Roman Igoshin <igoshin18@gmail.com>
2017-10-19 13:32:43 +0300

    add notifications

--- config/packages-to-notify.php
+++ config/packages-to-notify.php
@@ -4,7 +4,7 @@ return [
 
 	[
 		'url'                 => 'https://packagist.org/packages/masterro/laravel-file-cleaner',
-		'notify_on'           => [963, 998, 999],
+		'notify_on'           => [982, 983, 984, 985, 986, 987, 988, 989, 990, 991, 992, 993, 994, 995, 996, 997, 998, 999, 1000],
 		'notification_emails' => ['igoshin18@gmail.com'],
 		'notify_duplicates'   => false,
 	],
